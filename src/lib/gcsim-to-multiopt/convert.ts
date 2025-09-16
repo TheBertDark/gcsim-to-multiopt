1095d7f430ff2826b1e3ed912bd6e75d5480af21
Hemorragy <hemoragys@gmail.com>
2025-09-16 17:07:06 +0200

    Add new char abils

--- src/lib/gcsim-to-multiopt/convert.ts
+++ src/lib/gcsim-to-multiopt/convert.ts
@@ -159,6 +159,8 @@ export function mergeCustomTargets(targets: CustomTarget[]): CustomTarget[] {
 }
 
 export function convertAbils(abils: AbilInfo[], convert: AbilsType, charName?: string): [CustomMultiTarget, Error[]] {
+    if (charName)
+        charName.replace(/^(aeter|lumine)/, "traveler");
     const result = abils.map(x => convertAbil(x, convert, charName));
     const targets: CustomTarget[] = mergeCustomTargets(result.
         map(x => x[0]).
